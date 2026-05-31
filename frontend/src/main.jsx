5eb05993a669cab4c24d6671b93df63ad8217f2c
MohdShoeb1 <mohd.shoeb6306@gmail.com>
2026-05-31 10:58:22 +0530

    Add keep-alive functionality to main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { startKeepAlive } from './keepAlive.js'
+
+// Start pinging backend
+startKeepAlive();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
